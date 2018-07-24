a62c1be60e57050ea6c37768b442a2eb22029d1e
drgxx0 <alejandroff91@gmail.com>
2018-07-24 01:35:40 -0400

    new

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+	{
+		id: 173,
+		name: 'Veronica Gergen',
+		username: 'verogergen',
+		email: 'veroger@gmail.com'
+	},
+	{
+		id: 838,
+		name: 'August Terrain',
+		username: 'augusttt',
+		email: 'agtterr@gmail.com'
+	},
+	{
+		id: 352,
+		name: 'Angelik Kitas',
+		username: 'agelikitas',
+		email: 'angelk@gmail.com'
+	},
+	{
+		id: 4456,
+		name: 'Dieg Gate',
+		username: 'diegoot',
+		email: 'dgg@gmail.com'
+	},
+	{
+		id: 5767,
+		name: 'Melliss Seett',
+		username: 'mellisstt',
+		email: 'meltt@gmail.com'
+	},
+	{
+		id: 6234,
+		name: 'Zuuget Drog',
+		username: 'zugdrog',
+		email: 'zudrog@gmail.com'
+	},
+	{
+		id: 7454,
+		name: 'Yattek Katte',
+		username: 'yatkat',
+		email: 'yakkat@gmail.com'
+	},
+	{
+		id: 8234,
+		name: 'Hell Fire',
+		username: 'hellili',
+		email: 'hellfr@gmail.com'
+	},
+	{
+		id: 9655,
+		name: 'Latty Yatat',
+		username: 'lattyat',
+		email: 'laytat@gmail.com'
+	},
+	{
+		id: 10234,
+		name: 'Amanda Mandara',
+		username: 'amadara',
+		email: 'madara@gmail.com'
+	}
+]
